0fec4c262a447af0f3dec73209e6584a47280ccd
wignn <tigfiragnafatur1933@gmail.com>
2026-05-04 15:34:03 +0700

    Update WebSocket server binding to listen on all interfaces

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ async fn main() {
     let app = Router::new()
         .route("/ws/stats", get(ws_handler));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 6666));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 6666));
     println!("Listening on ws://{}", addr);
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
